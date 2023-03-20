45007f64add28bc3b3b4a339dd41032f3e30c244
Swiggityyy <ChrisAndCommunity@gmail.com>
2023-03-20 02:32:38 -0700

    asdf

--- website/src/components/Headercontact.js
+++ website/src/components/Headercontact.js
@@ -1,14 +1,19 @@
-export default function Headercontact () {
+export default function Headercontact() {
     return (
         <section data-toggle="collapse" data-target=".navbar-collapse">
-        <div class="container px-4 px-lg-5">
-            <div class="row gx-4 gx-lg-5">
-                <div class="col-lg-6">
-                    <h1 class="mt-5">Contact</h1>
-                    <p class="mt-4">Phone:</p>
+            <div class="container px-4 px-lg-5">
+                <div class="row gx-4 gx-lg-5">
+                    <div class="col-lg-6">
+                        <h1 class="mt-5">Contact</h1>
+                        <div class="mt-4">
+                            <p>Phone:</p>
+                            <p>Email:</p>
+                            <p>Address:</p>
+                        </div>
+
+                    </div>
                 </div>
             </div>
-        </div>
-    </section>
+        </section>
     )
 }
