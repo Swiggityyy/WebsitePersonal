bc7bb791101c4265a6fb52f67fd01aa7ee673b9c
Swiggityyy <ChrisAndCommunity@gmail.com>
2023-03-20 02:12:49 -0700

    asdf

--- website/src/components/Headercontact.js
+++ website/src/components/Headercontact.js
@@ -1,35 +1,24 @@
-export default function Headercontact () {
+export default function Headercontact() {
     return (
         <section data-toggle="collapse" data-target=".navbar-collapse">
-        <div class="container px-4 px-lg-5">
-            <div class="row gx-4 gx-lg-5">
-                <div class="col-lg-6">
-                    <h1 class="mt-5">Contact</h1>
-                    <p class="mt-4">
-                    Chris<br>
-                    (714)414-9723
-                    <br>
-                    ChrisAndUyenie@gmail.com
-                    <br>
-                    8432 Sandalwood Cir, Westminster CA 92683
-                    </br>
-                    </br>
-                    </br>
-                    </p>
-                    <p class="mt-4">
-                    Uyenie<br>
-                    (714)209-2332
-                    <br>
-                    ChrisAndUyenie@gmail.com
-                    <br>
-                    14421 Riverton Street, Westminster CA 92683
-                    </br>
-                    </br>
-                    </br>
-                    </p>
+            <div class="container px-4 px-lg-5">
+                <div class="row gx-4 gx-lg-5">
+                    <div class="col-lg-6">
+                        <h1 class="mt-5">Contact</h1>
+                        <p class="mt-4">
+                            Please send RSVP's and inquiries to<br>
+                                (714)414-9723
+                                <br>
+                                    ChrisAndUyenie@gmail.com
+                                    <br>
+                                        14412 Riverton St, Westminster CA 92683
+                                    </br>
+                                </br>
+                            </br>
+                        </p>
+                    </div>
                 </div>
             </div>
-        </div>
-    </section>
+        </section>
     )
 }
