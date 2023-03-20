3800083d1a84c2a508b2e74b1e0f04f2c1c87a8e
Swiggityyy <ChrisAndCommunity@gmail.com>
2023-03-19 23:28:47 -0700

    asdf

--- website/src/components/HeaderRSVP.js
+++ website/src/components/HeaderRSVP.js
@@ -11,24 +11,15 @@ export default function HeaderRSVP() {
 
             <div class="form-row">
                 <div class="col-md-4 mb-3">
-                    <label for="validationCustom01">First name</label>
+                    <label for="validationCustom01">Full Name</label>
                     <div class="input-group">
-                        <input type="text" class="form-control" id="validationCustom01" placeholder="First name" required />
+                        <input type="text" class="form-control" id="fullName" placeholder="John Doe" required />
                         <div class="valid-feedback">
                             Looks good!
                         </div>
                     </div>
                 </div>
-                <div class="col-md-4 mb-3">
-                    <label for="validationCustom02">Last name</label>
-                    <div class="input-group">
-                        <input type="text" class="form-control" id="validationCustom02" placeholder="Last name" required />
-                        <div class="valid-feedback">
-                            Looks good!
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-4 mb-3">
+                <div class="col-md-1 mb-3">
                     <label for="numGuests">Number of Guests</label>
                     <div class="input-group">
                         <input type="text" class="form-control" id="numGuests" placeholder="#Guests" required />
@@ -41,23 +32,23 @@ export default function HeaderRSVP() {
             <div class="form-row">
                 <div class="col-md-4 mb-3">
                     <label for="validationCustom03">City</label>
-                    <input type="text" class="form-control" id="validationCustom03" placeholder="City" required />
-                    <div class="invalid-feedback">
-                        Please provide a valid city.
-                    </div>
+                    <input type="text" class="form-control" id="validationCustom03" placeholder="City" />
                 </div>
                 <div class="col-md-3 mb-3">
                     <label for="validationCustom04">State</label>
-                    <input type="text" class="form-control" id="validationCustom04" placeholder="State" required />
-                    <div class="invalid-feedback">
-                        Please provide a valid state.
-                    </div>
+                    <input type="text" class="form-control" id="validationCustom04" placeholder="State" />
                 </div>
                 <div class="col-md-3 mb-3">
                     <label for="validationCustom05">Zip</label>
-                    <input type="text" class="form-control" id="validationCustom05" placeholder="Zip" required />
-                    <div class="invalid-feedback">
-                        Please provide a valid zip.
+                    <input type="text" class="form-control" id="validationCustom05" placeholder="Zip" />
+                </div>
+                <div class="col-md-8 mb-6">
+                    <label for="comment">Comments</label>
+                    <div class="input-group">
+                        <input type="text" class="form-control" id="comment" placeholder="Wedding Wishes!!!" />
+                        <div class="valid-feedback">
+                            Thank you!
+                        </div>
                     </div>
                 </div>
             </div>
