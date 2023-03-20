b2759af1c1c9594d6e6f583a5e9f207c5a94eb3c
Swiggityyy <ChrisAndCommunity@gmail.com>
2023-03-20 02:27:27 -0700

    asdf

--- website/src/components/Headercontact.js
+++ website/src/components/Headercontact.js
@@ -1,13 +1,14 @@
-
-
-export default function Headercontact() {
+export default function Headercontact () {
     return (
-        <div class="card">
-            <img src="https://chrisanduyenie.s3.us-west-1.amazonaws.com/images/ChrisUyenie_RVP110.jpg" alt="Avatar" style="width:100%" />
-            <div class="container">
-                <h4><b>John Doe</b></h4>
-                <p>Architect & Engineer</p>
+        <section data-toggle="collapse" data-target=".navbar-collapse">
+        <div class="container px-4 px-lg-5">
+            <div class="row gx-4 gx-lg-5">
+                <div class="col-lg-6">
+                    <h1 class="mt-5">Contact</h1>
+                    <p></p>
+                </div>
             </div>
         </div>
+    </section>
     )
 }
