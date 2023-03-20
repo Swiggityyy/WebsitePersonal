cd7ae1937e3aaba3baaf42f1143efe00db400708
Swiggityyy <ChrisAndCommunity@gmail.com>
2023-03-20 03:26:36 -0700

    sdaf

--- website/src/components/Headercalendar.js
+++ website/src/components/Headercalendar.js
@@ -1,5 +1,3 @@
-import { VerticalTimeline, VerticalTimelineElement } from '../react-vertical-timeline-component/';
-import '../react-vertical-timeline-component/style.min.css';
 
 export default function Headercalendar() {
     return (
