9e3e74e5361ab01da474025c4c79091fdef332dd
Swiggityyy <ChrisAndCommunity@gmail.com>
2023-03-20 06:09:35 -0700

    asdf

--- website/src/pages/Contact.js
+++ website/src/pages/Contact.js
@@ -1,11 +1,9 @@
 import Headercontact from "../components/Headercontact"
-import CommentBox from "../components/CommentBox"
 
 export default function Contact() {
     return (
         <>
             <Headercontact />
-            <CommentBox />
         </>
     )
 }
