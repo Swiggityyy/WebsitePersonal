a183fe2fc77b7cfa6f8f0663d42b309af2ee33e9
Swiggityyy <ChrisAndCommunity@gmail.com>
2023-03-20 02:17:25 -0700

    sadf

--- website/src/components/Headercontact.js
+++ website/src/components/Headercontact.js
@@ -1,13 +1,11 @@
 export default function Headercontact() {
     return (
-        <section data-toggle="collapse" data-target=".navbar-collapse">
-            <div class="card">
-                <img src="img_avatar.png" alt="Avatar" style="width:100%" />
-                <div class="container">
-                    <h4><b>John Doe</b></h4>
-                    <p>Architect & Engineer</p>
-                </div>
+        <div class="card">
+            <img src="img_avatar.png" alt="Avatar" style="width:100%" />
+            <div class="container">
+                <h4><b>John Doe</b></h4>
+                <p>Architect & Engineer</p>
             </div>
-        </section>
+        </div>
     )
 }
