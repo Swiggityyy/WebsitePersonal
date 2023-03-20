2293d8b28aa7caa33704c18640b66f1591de7f88
Swiggityyy <ChrisAndCommunity@gmail.com>
2023-03-20 05:58:32 -0700

    asdf

---
+++ website/src/components/CommentBox.js
@@ -0,0 +1,12 @@
+export default function Comment() {
+    <div class="containerChat">
+        <h1 class="mt-5">Contact</h1>
+        <div class="mt-4">
+            <p>Phone: 714-414-9723</p>
+            <p>Email: ChrisAndUyenie@gmail.com</p>
+            <p>Mailing: 14421 Riverton St</p>
+            <p>Westminster CA 92683</p>
+        </div>
+    </div>
+
+}
--- website/src/components/Headercontact.js
+++ website/src/components/Headercontact.js
@@ -14,19 +14,6 @@ export default function Headercontact() {
                     </div>
                 </div>
             </div>
-            <div class="containerChat">
-            <div class="rowChat">
-                <div class="colChat">
-                    <h1 class="mt-5">Contact</h1>
-                    <div class="mt-4">
-                        <p>Phone: 714-414-9723</p>
-                        <p>Email: ChrisAndUyenie@gmail.com</p>
-                        <p>Mailing: 14421 Riverton St</p>
-                        <p>Westminster CA 92683</p>
-                    </div>
-                </div>
-            </div>
-        </div>
         </section>
         
     )
