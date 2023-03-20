e4052705b097889da4ab30699a470a0f47a8cd1e
Swiggityyy <ChrisAndCommunity@gmail.com>
2023-03-20 01:59:00 -0700

    sadf

--- website/src/components/Headercontact.js
+++ website/src/components/Headercontact.js
@@ -1,14 +1,21 @@
-export default function Headercontact () {
+export default function Headercontact() {
     return (
         <section data-toggle="collapse" data-target=".navbar-collapse">
-        <div class="container px-4 px-lg-5">
-            <div class="row gx-4 gx-lg-5">
-                <div class="col-lg-6">
-                    <h1 class="mt-5">Contact</h1>
-                    <p></p>
-                </div>
+            <div class="popup">
+                <h1>Contact</h1>
+                <p>Chris
+                Phone: (714)414-9723
+                email: ChrisAndUyenie@gmail.com
+                Address: 8432 Sandalwood Cir <br>
+                Westminster CA 92683</br>
+
+                Uyenie
+                Phone: (714)209-2332
+                email: ChrisAndUyenie@gmail.com
+                Address: 14412 Riverton St <br>
+                Westminster CA 92683</br>                
+                </p>
             </div>
-        </div>
-    </section>
+        </section>
     )
 }
