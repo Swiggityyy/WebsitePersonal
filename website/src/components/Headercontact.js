dd9bdde4acdc78c669640afb82bda492cf3ea7de
Swiggityyy <ChrisAndCommunity@gmail.com>
2023-03-20 02:20:06 -0700

    asdf

--- website/src/components/Headercontact.js
+++ website/src/components/Headercontact.js
@@ -1,7 +1,9 @@
+
+
 export default function Headercontact() {
     return (
         <div class="card">
-            <img src="img_avatar.png" alt="Avatar" style="width:100%" />
+            <img src="https://chrisanduyenie.s3.us-west-1.amazonaws.com/images/ChrisUyenie_RVP110.jpg" alt="Avatar" style="width:100%" />
             <div class="container">
                 <h4><b>John Doe</b></h4>
                 <p>Architect & Engineer</p>
