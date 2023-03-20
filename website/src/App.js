c3ce1358945904dbb9b2580d95ee54222a83a481
Swiggityyy <ChrisAndCommunity@gmail.com>
2023-03-19 21:23:29 -0700

    asdf

--- website/src/App.js
+++ website/src/App.js
@@ -4,8 +4,9 @@ import Calendar from './pages/Calendar';
 import Contact from './pages/Contact';
 import Error from './pages/Error';
 import Homepage from './pages/Homepage';
-import Info from './pages/RSVP'
+import RSVP from './pages/RSVP'
 import Headerphoto from './components/Headerphoto';
+import RSVP from './pages/RSVP';
 
 function App() {
   return (
@@ -13,7 +14,7 @@ function App() {
     <BrowserRouter>
     <Routes>
     <Route index element={<Homepage />} />
-    <Route path='/info' element={<Info />} />
+    <Route path='/rsvp' element={<RSVP />} />
     <Route path='/calendar' element={<Calendar />} />
     <Route path='/contact' element={<Contact />} />
     <Route path='/photos' element={<Headerphoto />} />
