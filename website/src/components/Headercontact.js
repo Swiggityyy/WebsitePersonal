bc770ee882f16833ee21202afcd623c01466040b
Swiggityyy <ChrisAndCommunity@gmail.com>
2023-03-20 02:02:12 -0700

    asdf

--- website/src/components/Headercontact.js
+++ website/src/components/Headercontact.js
@@ -1,21 +1,26 @@
-export default function Headercontact() {
+export default function Headercontact () {
     return (
-        <section class="popup" data-toggle="collapse" data-target=".navbar-collapse">
-            <div class="mt-5">
-                <h1>Contact</h1>
-                <p>Chris
-                Phone: (714)414-9723
-                email: ChrisAndUyenie@gmail.com
-                Address: 8432 Sandalwood Cir <br>
-                Westminster CA 92683</br>
-
-                Uyenie
-                Phone: (714)209-2332
-                email: ChrisAndUyenie@gmail.com
-                Address: 14412 Riverton St <br>
-                Westminster CA 92683</br>                
-                </p>
+        <section data-toggle="collapse" data-target=".navbar-collapse">
+        <div class="container px-4 px-lg-5">
+            <div class="row gx-4 gx-lg-5">
+                <div class="col-lg-6">
+                    <h1 class="mt-5">Contact</h1>
+                    <p class="mt-4">Chris
+                    Phone: (714)414-9723
+                    email: ChrisAndUyenie@gmail.com
+                    Address: 8432 Sandalwood Cir <br>
+                    Westminster CA 92683</br>
+    
+                    Uyenie
+                    Phone: (714)209-2332
+                    email: ChrisAndUyenie@gmail.com
+                    Address: 14412 Riverton St <br>
+                    Westminster CA 92683</br>                
+                    </p>
+                    <p></p>
+                </div>
             </div>
-        </section>
+        </div>
+    </section>
     )
 }
