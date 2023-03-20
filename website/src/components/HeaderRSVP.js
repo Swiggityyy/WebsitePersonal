25918fe57a6fd15e1d1e97e2e8d04a32e85a088a
Swiggityyy <ChrisAndCommunity@gmail.com>
2023-03-19 22:51:32 -0700

    asdf

--- website/src/components/HeaderRSVP.js
+++ website/src/components/HeaderRSVP.js
@@ -2,9 +2,15 @@ import React from "react"
 
 export default function HeaderRSVP() {
     return (
-        <form name="contact" netlify>
+        <form name="contact" classname="rsvpcomment" netlify>
             <p>
                 <label>Name <input type="text" name="name" /></label>
+            </p>
+            <p>
+                <label>Email <input type="email" name="email" /></label>
+            </p>
+            <p>
+                <label>Email <input type="email" name="email" /></label>
             </p>
             <p>
                 <label>Email <input type="email" name="email" /></label>
