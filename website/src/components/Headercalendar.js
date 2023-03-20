97fa56f37a3645479e6baa1b1ca66640d8f3a6d9
Swiggityyy <ChrisAndCommunity@gmail.com>
2023-03-20 04:52:31 -0700

    asdf

--- website/src/components/Headercalendar.js
+++ website/src/components/Headercalendar.js
@@ -3,99 +3,99 @@
 export default function Headercalendar() {
     return (
         <div class="timeline">
-            <div class="container left">
-                <div class="content">
+            <div class="containercalendar leftcalendar">
+                <div class="contentcalendar">
                     <h2>March 23rd 2021</h2>
                     <p>We start talking through messenger while Chris
                         was on the road as a trucker.</p>
                 </div>
             </div>
-            <div class="container right">
-                <div class="content">
+            <div class="containercalendar rightcalendar">
+                <div class="contentcalendar">
                     <h2>June 20th 2021</h2>
                     <p>Chris comes home and they go
                         on their first date. Chris tricks
                         Uyenie into watching a Quiet Place II</p>
                 </div>
             </div>
-            <div class="container left">
-                <div class="content">
+            <div class="containercalendar leftcalendar">
+                <div class="contentcalendar">
                     <h2>July 4th 2021</h2>
                     <p>Chris and Uyenie go to Griffith
                         Observatory to watch the fireworks
                         with their middle school friends</p>
                 </div>
             </div>
-            <div class="container right">
-                <div class="content">
+            <div class="containercalendar rightcalendar">
+                <div class="contentcalendar">
                     <h2>September 13th 2021</h2>
                     <p>They get really involved in the
                         community. It's tiring but rewarding.</p>
                 </div>
             </div>
-            <div class="container left">
-                <div class="content">
+            <div class="containercalendar leftcalendar">
+                <div class="contentcalendar">
                     <h2>October 30th 2021</h2>
                     <p>Chris shows Uyenie go to the Aquarium</p>
                 </div>
             </div>
-            <div class="container right">
-                <div class="content">
+            <div class="containercalendar rightcalendar">
+                <div class="contentcalendar">
                     <h2>Nov 29th 2021</h2>
                     <p>Chris and Uyenie go to Riverside to
                         See the festival of lights. Also Encanto!</p>
                 </div>
             </div>
-            <div class="container left">
-                <div class="content">
+            <div class="containercalendar leftcalendar">
+                <div class="contentcalendar">
                     <h2>December 18th 2021</h2>
                     <p>Uyenie adopts a pet snail named SNELLY!!!!</p>
                 </div>
             </div>
-            <div class="container right">
-                <div class="content">
+            <div class="containercalendar rightcalendar">
+                <div class="contentcalendar">
                     <h2>February 2022</h2>
                     <p>Tet Festival :D</p>
                 </div>
             </div>
-            <div class="container left">
-                <div class="content">
+            <div class="containercalendar leftcalendar">
+                <div class="contentcalendar">
                     <h2>March 11th 2022</h2>
                     <p>Chris and Uyenie get Disney passes.</p>
                 </div>
             </div>
-            <div class="container right">
-                <div class="content">
+            <div class="containercalendar rightcalendar">
+                <div class="contentcalendar">
                     <h2>July 8th 2022</h2>
                     <p>Chris and Uyenie go to see the Moulin Rouge!</p>
                 </div>
             </div>
-            <div class="container left">
-                <div class="content">
+            <div class="containercalendar leftcalendar">
+                <div class="contentcalendar">
                     <h2>October 27th 2022</h2>
                     <p>Chris proposes!!!</p>
                 </div>
             </div>
-            <div class="container right">
-                <div class="content">
+            <div class="containercalendar rightcalendar">
+                <div class="contentcalendar">
                     <h2>January 2023</h2>
                     <p>Tet Festival D:</p>
                 </div>
             </div>
-            <div class="container left">
-                <div class="content">
+            <div class="containercalendar leftcalendar">
+                <div class="contentcalendar">
                     <h2>February 18th 2023</h2>
                     <p>Our engagement party!</p>
                 </div>
             </div>
-            <div class="container right">
-                <div class="content">
+            <div class="containercalendar rightcalendar">
+                <div class="contentcalendar">
                     <h2>March 20th 2023</h2>
                     <p>Chris makes a website :3</p>
                 </div>
             </div>
-            <div class="container left">
-                <div class="content">
+            <div class="containercalendar leftcalendar">
+                <div class="contentcalendar">
                     <h2>December 16th 2023</h2>
                     <p>Chris and Uyenie' wedding day!</p>
                 </div>
