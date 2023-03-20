b8f7546dd72e9ca53a0b7234f528b0ee0f0e9380
Swiggityyy <ChrisAndCommunity@gmail.com>
2023-03-20 02:29:36 -0700

    asdf

--- website/src/components/Headercontact.js
+++ website/src/components/Headercontact.js
@@ -4,7 +4,14 @@ export default function Headercontact () {
         <div class="container px-4 px-lg-5">
             <div class="row gx-4 gx-lg-5">
                 <div class="col-lg-6">
-                    <h1 class="mt-5">Contact</h1>
+                    <h1 class="mt-5"> <div class="card">
+                    <img src="https://chrisanduyenie.s3.us-west-1.amazonaws.com/images/ChrisUyenie_RVP81.jpg" alt="Avatar" style="width:100%"/>
+                    <div class="container">
+                      <h4><b>John Doe</b></h4>
+                      <p>Architect & Engineer</p>
+                    </div>
+                  </div> 
+                  </h1>
                     <p></p>
                 </div>
             </div>
