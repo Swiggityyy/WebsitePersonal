17e2cef3ac5543bc505ba0232228c875dfde18cf
Swiggityyy <ChrisAndCommunity@gmail.com>
2023-03-23 17:59:52 -0700

    asdf

--- website/src/components/HeaderHome.js
+++ website/src/components/HeaderHome.js
@@ -1,14 +1,9 @@
-export default function HeaderHome () {
+export default function HeaderHome() {
     return (
-        <section data-toggle="collapse" data-target=".navbar-collapse">
-        <div class="container px-4 px-lg-5">
-            <div class="row gx-4 gx-lg-5">
-                <div class="col-lg-6">
-                    <h1 class="mt-5">Chris and Uyenie's Wedding</h1>
-                    <p class="mt-5">A site dedicated to us.</p>
-                </div>
-            </div>
+        <div class="col-lg-6">
+            <h1 class="mt-5">Chris and Uyenie's Wedding</h1>
+            <p class="mt-5">A site dedicated to us.</p>
         </div>
-    </section>
+
     )
 }
