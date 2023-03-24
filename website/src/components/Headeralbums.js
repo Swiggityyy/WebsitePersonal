fb9500d194395dd8a4460884dd7b8e5c833d9347
Swiggityyy <ChrisAndCommunity@gmail.com>
2023-03-23 17:46:51 -0700

    asdf

--- website/src/components/Headeralbums.js
+++ website/src/components/Headeralbums.js
@@ -4,13 +4,8 @@ export default function Headeralbums() {
             <div class="container px-4 px-lg-5">
                 <div class="row gx-4 gx-lg-5">
                     <div class="col-lg-6">
-                        <h1 class="mt-5">Contact</h1>
-                        <div class="mt-4">
-                            <p>Phone: 714-414-9723</p>
-                            <p>Email: ChrisAndUyenie@gmail.com</p>
-                            <p>Mailing: 14421 Riverton St</p>
-                            <p>Westminster CA 92683</p>
-                        </div>
+                        <h1 class="mt-5" href="https://drive.google.com/drive/folders/1klPmnxaruhEUianp6T3LIMzzOF1omhvi?usp=share_link">Engagement</h1>
+                        
                     </div>
                 </div>
             </div>
