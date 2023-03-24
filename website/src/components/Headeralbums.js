2f0f507cbe03eb7a2a0e12d4dd7cc256d3b18e3b
Swiggityyy <ChrisAndCommunity@gmail.com>
2023-03-23 17:57:10 -0700

    asdf

--- website/src/components/Headeralbums.js
+++ website/src/components/Headeralbums.js
@@ -1,5 +1,14 @@
 export default function Headeralbums() {
     return (
-       <h1 href="https://drive.google.com/drive/folders/1klPmnxaruhEUianp6T3LIMzzOF1omhvi?usp=share_link">asdf</h1>
+        <section data-toggle="collapse" data-target=".navbar-collapse">
+            <div class="container px-4 px-lg-5">
+                <div class="row gx-4 gx-lg-5">
+                    <div class="col-lg-6">
+                        <h1 id="https://drive.google.com/drive/folders/1klPmnxaruhEUianp6T3LIMzzOF1omhvi?usp=share_link" class="mt-5">Engagement</h1>
+                    </div>
+                </div>
+            </div>
+        </section>
+        
     )
 }
