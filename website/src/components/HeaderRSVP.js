740c6b0963563af0c0f3119ccbb6ee30139c885e
Swiggityyy <ChrisAndCommunity@gmail.com>
2023-03-23 20:51:59 -0700

    asdf

--- website/src/components/HeaderRSVP.js
+++ website/src/components/HeaderRSVP.js
@@ -18,29 +18,23 @@ export default function HeaderRSVP() {
             <div class="form-row">
                 <div class="col-md-4 mb-2">
                     <label htmlFor="fullName">Full Name</label>
-                    <div class="input-group">
-                        <input type="text" name="fullName" class="form-control" id="fullName" placeholder="John Doe" required />
-                        <div class="invalid-feedback">
-                            Please enter a name!
-                        </div>
+                    <input type="text" name="fullName" class="form-control" id="fullName" placeholder="John Doe" required />
+                    <div class="invalid-feedback">
+                        Please enter a name!
                     </div>
                 </div>
                 <div class="col-md-4 mb-2">
                     <label htmlFor="email">Email</label>
-                    <div class="input-group">
-                        <input type="email" name="email" class="form-control" id="email" placeholder="JohnDoe@gmail.com" required />
-                        <div class="invalid-feedback">
-                            Please enter an email!
-                        </div>
+                    <input type="email" name="email" class="form-control" id="email" placeholder="JohnDoe@gmail.com" required />
+                    <div class="invalid-feedback">
+                        Please enter an email!
                     </div>
                 </div>
                 <div class="col-md-4 mb-2">
                     <label htmlFor="phone">Phone: 123-456-7890</label>
-                    <div class="input-group">
-                        <input type="tel" pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" name="phone" class="form-control" id="phone" placeholder="714-123-1234" required />
-                        <div class="invalid-feedback">
-                            Please provide a working phone number.
-                        </div>
+                    <input type="tel" pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" name="phone" class="form-control" id="phone" placeholder="714-123-1234" required />
+                    <div class="invalid-feedback">
+                        Please provide a working phone number.
                     </div>
                 </div>
                 {/* <div class="col-md-4 mb-2">
@@ -62,15 +56,11 @@ export default function HeaderRSVP() {
                 </div>
                 <div class="col-md-6 mb-2">
                     <label htmlFor="pNames">Names of People in Party</label>
-                    <div class="input-group">
-                        <textarea type="text" name="pNames" id="pNames" class="form-control" placeholder="John Nguyen, Susie Nguyen, Brittany Truong" />
-                    </div>
+                    <textarea type="text" name="pNames" id="pNames" class="form-control" placeholder="John Nguyen, Susie Nguyen, Brittany Truong" />
                 </div>
                 <div class="col-md-6 mb-2">
                     <label htmlFor="comment">Comments and Blessings</label>
-                    <div class="input-group">
-                        <textarea type="text" name="comment" id="comment" class="form-control" placeholder="Dear Chris and Uyenie, you guys are awesome. We are grateful to have you in our lives. -Love, the best relative ever :)" />
-                    </div>
+                    <textarea type="text" name="comment" id="comment" class="form-control" placeholder="Dear Chris and Uyenie, you guys are awesome. We are grateful to have you in our lives. -Love, the best relative ever :)" />
                 </div>
             </div>
             <button class="btn btn-primary" type="submit">Submit form</button>
