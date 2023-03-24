0fd6bc8c5d02aed39de8563c3a2ac77086b57c31
Swiggityyy <ChrisAndCommunity@gmail.com>
2023-03-23 18:24:54 -0700

    asdf

--- website/src/components/Headeralbums.js
+++ website/src/components/Headeralbums.js
@@ -5,10 +5,12 @@ export default function Headeralbums() {
                 <div class="row gx-4 gx-lg-5">
                     <div class="col-lg-6">
                         <h1 class="mt-5"><a href="https://drive.google.com/drive/folders/1klPmnxaruhEUianp6T3LIMzzOF1omhvi?usp=share_link">Engagement</a></h1>
+                        <h1 class="mt-5"><a href="https://drive.google.com/drive/folders/1ZcRIiUzqMp9ESetrxMUbqPogS4eCGzh9?usp=share_link">One Year Anniversary</a></h1>
+                        <h1 class="mt-5"><a href="https://drive.google.com/drive/folders/1SwP5CqHqHgwnS1JHSj233WOZxffscKxB?usp=share_link">Proposal</a></h1>
                     </div>
                 </div>
             </div>
         </section>
-        
+
     )
 }
