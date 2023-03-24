a45de4efba0d116b0eec15090d633ea50a390639
Swiggityyy <ChrisAndCommunity@gmail.com>
2023-03-23 18:08:14 -0700

    adfs

--- website/src/components/Headeralbums.js
+++ website/src/components/Headeralbums.js
@@ -1,10 +1,10 @@
 export default function Headeralbums() {
     return (
-        <section data-toggle="collapse" data-target=".navbar-collapse">
+        <section data-toggle="collapse" data-target=".navbar-collapse" id="">
             <div class="container px-4 px-lg-5">
                 <div class="row gx-4 gx-lg-5">
                     <div class="col-lg-6">
-                        <h1 id="https://drive.google.com/drive/folders/1klPmnxaruhEUianp6T3LIMzzOF1omhvi?usp=share_link" class="mt-5">Engagement</h1>
+                        <h1 class="mt-5"><a href="https://drive.google.com/drive/folders/1klPmnxaruhEUianp6T3LIMzzOF1omhvi?usp=share_link">Engagement</a></h1>
                     </div>
                 </div>
             </div>
