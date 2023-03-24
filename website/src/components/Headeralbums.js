d131e61f120614ce4da5721db0b4a9fafa3f11aa
Swiggityyy <ChrisAndCommunity@gmail.com>
2023-03-23 17:54:17 -0700

    asdf

--- website/src/components/Headeralbums.js
+++ website/src/components/Headeralbums.js
@@ -1,5 +1,5 @@
 export default function Headeralbums() {
     return (
-       <h1>asdf</h1>
+       <h1 href="https://drive.google.com/drive/folders/1klPmnxaruhEUianp6T3LIMzzOF1omhvi?usp=share_link">asdf</h1>
     )
 }
