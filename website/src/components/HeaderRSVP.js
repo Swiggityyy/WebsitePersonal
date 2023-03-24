314b189f01642a69928069422d6eeff90cc0b88c
Swiggityyy <ChrisAndCommunity@gmail.com>
2023-03-23 21:06:38 -0700

    asdf

--- website/src/components/HeaderRSVP.js
+++ website/src/components/HeaderRSVP.js
@@ -18,40 +18,40 @@ export default function HeaderRSVP() {
             <div class="form-row">
                 <div class="col-md-4 mb-2">
                     <label htmlFor="fullName">Full Name</label>
-                    <input type="text" name="fullName" class="form-control" id="fullName" placeholder="John Doe" required />
+                    <input type="text" name="fullName" id="fullName" placeholder="John Doe" required />
                     <div class="invalid-feedback">
                         Please enter a name!
                     </div>
                 </div>
                 <div class="col-md-4 mb-2">
                     <label htmlFor="email">Email</label>
-                    <input type="email" name="email" class="form-control" id="email" placeholder="JohnDoe@gmail.com" required />
+                    <input type="email" name="email" id="email" placeholder="JohnDoe@gmail.com" required />
                     <div class="invalid-feedback">
                         Please enter an email!
                     </div>
                 </div>
                 <div class="col-md-4 mb-2">
                     <label htmlFor="phone">Phone: 123-456-7890</label>
-                    <input type="tel" pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" name="phone" class="form-control" id="phone" placeholder="714-123-1234" required />
+                    <input type="tel" pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" name="phone" id="phone" placeholder="714-123-1234" required />
                     <div class="invalid-feedback">
                         Please provide a working phone number.
                     </div>
                 </div>
                 <div class="col-md-4 mb-2">
                     <label htmlFor="country">Country</label>
-                    <input type="text" name="country" class="form-control" id="country" placeholder="Canada/US" />
+                    <input type="text" name="country" id="country" placeholder="Canada/US" />
                 </div>
                 <div class="col-md-4 mb-2">
                     <label htmlFor="address">Full Address</label>
-                    <input type="text" name="address" class="form-control" id="address" placeholder="12345 Fake Street, Imaginary City CA 92683" />
+                    <input type="text" name="address" id="address" placeholder="12345 Fake Street, Imaginary City CA 92683" />
                 </div>
                 <div class="col-md-6 mb-2">
                     <label htmlFor="party">Names of People in Party</label>
-                    <textarea type="text" name="party" class="form-control" id="party" placeholder="John Nguyen, Susie Nguyen, Brittany Truong" />
+                    <textarea type="text" name="party" id="party" placeholder="John Nguyen, Susie Nguyen, Brittany Truong" />
                 </div>
                 <div class="col-md-6 mb-2">
                     <label htmlFor="comment">Comments and Blessings</label>
-                    <textarea type="text" name="comment" class="form-control" id="comment" placeholder="Dear Chris and Uyenie, you guys are awesome. We are grateful to have you in our lives. -Love, the best relative ever :)" />
+                    <textarea type="text" name="comment" id="comment" placeholder="Dear Chris and Uyenie, you guys are awesome. We are grateful to have you in our lives. -Love, the best relative ever :)" />
                 </div>
             </div>
             <button class="btn btn-primary" type="submit">Submit form</button>
