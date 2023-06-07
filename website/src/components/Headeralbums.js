40d18d8a28a5544c728fa575f01f50b6cd7cc9a8
Swiggityyy <ChrisAndCommunity@gmail.com>
2023-06-06 18:42:57 -0700

    asdf

--- website/src/components/Headeralbums.js
+++ website/src/components/Headeralbums.js
@@ -4,7 +4,8 @@ export default function Headeralbums() {
             <div class="container px-4 px-lg-5">
                 <div class="row gx-4 gx-lg-5">
                     <div class="col-lg-6">
-                        <h1 class="mt-5"><a href="https://drive.google.com/drive/folders/1klPmnxaruhEUianp6T3LIMzzOF1omhvi?usp=share_link" target="_blank" >Engagement</a></h1>
+                        <h1 class="mt-5"><a href="https://drive.google.com/drive/folders/1klPmnxaruhEUianp6T3LIMzzOF1omhvi?usp=share_link" target="_blank" >Engagement Party</a></h1>
+                        <h1 class="mt-5"><a href="https://photos.app.goo.gl/96uq6WjLbfShCPDy7" target="_blank" >Engagement Photos</a></h1>
                     </div>
                 </div>
             </div>
