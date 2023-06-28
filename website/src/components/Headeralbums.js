8402ec6e0d61a031989d462b43d46bd285bbec2d
Swiggityyy <ChrisAndCommunity@gmail.com>
2023-06-28 05:03:39 -0700

    asdf

--- website/src/components/Headeralbums.js
+++ website/src/components/Headeralbums.js
@@ -4,9 +4,9 @@ export default function Headeralbums() {
             <div class="container px-4 px-lg-5">
                 <div class="row gx-4 gx-lg-5">
                     <div class="col-lg-6">
-                        <img src="https://chrisanduyenie.s3.us-west-1.amazonaws.com/images/ChrisUyenie_RVP51.jpg" style="height:10%;width:10%"></img>
+                        <img src="https://chrisanduyenie.s3.us-west-1.amazonaws.com/images/ChrisUyenie_RVP51.jpg" style="height:10px;width:10px"></img>
                         <h1 class="mt-5"><a href="https://drive.google.com/drive/folders/1klPmnxaruhEUianp6T3LIMzzOF1omhvi?usp=share_link" target="_blank" >Engagement Party</a></h1>
-                        <img src="https://chrisanduyenie.s3.us-west-1.amazonaws.com/engagement/ALV00639.jpg" style="height:10%;width:10%"></img>
+                        <img src="https://chrisanduyenie.s3.us-west-1.amazonaws.com/engagement/ALV00639.jpg" style="height:10px;width:10px"></img>
                         <h1 class="mt-5"><a href="https://photos.app.goo.gl/96uq6WjLbfShCPDy7" target="_blank" >Engagement Photos</a></h1>
                     </div>
                 </div>
