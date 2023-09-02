a1c5f5135c390b0803dfb7d01e0ab398549a0884
Swiggityyy <ChrisAndCommunity@gmail.com>
2023-09-02 16:12:34 -0700

    asdf

--- website/src/pages/RSVP.js
+++ website/src/pages/RSVP.js
@@ -1,8 +1,8 @@
-// import HeaderRSVP from "../components/HeaderRSVP"
-// export default function RSVP() {
-//     return (
-//         <>
-//         <HeaderRSVP />
-//         </>
-//     )
-// }
+import HeaderRSVP from "../components/HeaderRSVP"
+export default function RSVP() {
+    return (
+        <>
+        <HeaderRSVP />
+        </>
+    )
+}
