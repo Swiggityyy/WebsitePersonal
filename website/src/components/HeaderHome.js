559c207c7758a882c73f4ac7608a6de91cb554a4
Swiggityyy <ChrisAndCommunity@gmail.com>
2023-10-21 21:37:08 -0700

    asdf

--- website/src/components/HeaderHome.js
+++ website/src/components/HeaderHome.js
@@ -8,6 +8,7 @@ export default function HeaderHome () {
                     <p class="mt-5" color="grey">A site dedicated to us.</p>
                     <p class="mt-5" color="grey">Golden Sea Restaurant</p>
                     <p class="mt-5" color="grey">9802 Katella Ave, Anaheim, CA 92804</p>
+                    <p class="mt-5" color="grey">December 16th 2023</p>
                 </div>
             </div>
         </div>
