6c063ca174c64732d88dded761ba25888a585203
Swiggityyy <chrisandcommunity@gmail.com>
2024-07-20 10:32:26 -0700

    asdf

--- website/src/components/Headerphoto.js
+++ website/src/components/Headerphoto.js
@@ -97,5 +97,6 @@ export default function Headerphoto() {
 
 //     )
 
-<iframe width="560" height="315" src="https://www.youtube.com/embed/S04-Ys_H7AY?si=vAxOaxYU5bhcmq6K" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
-};
+<iframe width="420" height="315"
+src="https://www.youtube.com/embed/tgbNymZ7vqY?controls=0">
+</iframe>};
