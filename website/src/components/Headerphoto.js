89457a5cd1cbee21b23f3222b644194ac4e90c19
Swiggityyy <chrisandcommunity@gmail.com>
2024-07-20 10:29:26 -0700

    asdf

--- website/src/components/Headerphoto.js
+++ website/src/components/Headerphoto.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ImageGallery from 'react-image-gallery';
+// import ImageGallery from 'react-image-gallery';
 import "./react-image-gallery/styles/css/image-gallery.css";
 
 
