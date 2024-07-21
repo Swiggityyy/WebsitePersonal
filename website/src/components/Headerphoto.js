955aeeead22ecc5d596dd919e299e2c0935c1b5d
Swiggityyy <chrisandcommunity@gmail.com>
2024-07-21 13:13:07 -0700

    asdf

--- website/src/components/Headerphoto.js
+++ website/src/components/Headerphoto.js
@@ -86,14 +86,16 @@ export default function Headerphoto() {
     
     return (
         <div>
-            <ImageGallery items={images}
+            {<iframe width="560" height="315" src="https://www.youtube.com/embed/S04-Ys_H7AY?si=a2vJimMwVB8gWY_W" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+            
+            /* <ImageGallery items={images}
                 startIndex={4}
                 showPlayButton={false}
                 showFullscreenButton={true}
                 lazyLoad={true}
                 thumbnailPosition='right'
                 useBrowserFullscreen={true}
-            />;
+            />; */}
         </div>
 
     )
