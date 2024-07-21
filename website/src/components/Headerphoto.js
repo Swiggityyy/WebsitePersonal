508841dca42b3071d3057eab5c1eb86aab7e000d
Swiggityyy <chrisandcommunity@gmail.com>
2024-07-21 13:10:05 -0700

    asdf

--- website/src/components/Headerphoto.js
+++ website/src/components/Headerphoto.js
@@ -5,109 +5,99 @@ import "./react-image-gallery/styles/css/image-gallery.css";
 import { Container } from 'react-bootstrap'
 
 export default function Headerphoto() {
-    return <div className='ratio ratio-16x9'>
-        <Container>
-            <iframe width="560" height="315" src="https://www.youtube.com/embed/S04-Ys_H7AY?si=gPIswrki984OTNp1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
-        </Container>
-    </div>
 
+// let random = Math.floor((Math.random() * 50) + 1); 
 
-
+    const images = [
+        {
+            original: 'https://chrisanduyenie.s3.us-west-1.amazonaws.com/Mainpage/ALV00612.jpg',
+            thumbnail: 'https://chrisanduyenie.s3.us-west-1.amazonaws.com/Mainpage/ALV00612.jpg',
+            },
+            {
+            original: 'https://chrisanduyenie.s3.us-west-1.amazonaws.com/Mainpage/ALV00643.jpg',
+            thumbnail: 'https://chrisanduyenie.s3.us-west-1.amazonaws.com/Mainpage/ALV00643.jpg',
+            },
+            {
+            original: 'https://chrisanduyenie.s3.us-west-1.amazonaws.com/Mainpage/ALV00686.jpg',
+            thumbnail: 'https://chrisanduyenie.s3.us-west-1.amazonaws.com/Mainpage/ALV00686.jpg',
+            },
+            {
+            original: 'https://chrisanduyenie.s3.us-west-1.amazonaws.com/Mainpage/ALV00704.jpg',
+            thumbnail: 'https://chrisanduyenie.s3.us-west-1.amazonaws.com/Mainpage/ALV00704.jpg',
+            },
+            {
+            original: 'https://chrisanduyenie.s3.us-west-1.amazonaws.com/Mainpage/ALV00739.jpg',
+            thumbnail: 'https://chrisanduyenie.s3.us-west-1.amazonaws.com/Mainpage/ALV00739.jpg',
+            },
+            {
+            original: 'https://chrisanduyenie.s3.us-west-1.amazonaws.com/Mainpage/ALV00770.jpg',
+            thumbnail: 'https://chrisanduyenie.s3.us-west-1.amazonaws.com/Mainpage/ALV00770.jpg',
+            },
+            {
+            original: 'https://chrisanduyenie.s3.us-west-1.amazonaws.com/Mainpage/ALV00790.jpg',
+            thumbnail: 'https://chrisanduyenie.s3.us-west-1.amazonaws.com/Mainpage/ALV00790.jpg',
+            },
+            {
+            original: 'https://chrisanduyenie.s3.us-west-1.amazonaws.com/Mainpage/ALV00837.jpg',
+            thumbnail: 'https://chrisanduyenie.s3.us-west-1.amazonaws.com/Mainpage/ALV00837.jpg',
+            },
+            {
+            original: 'https://chrisanduyenie.s3.us-west-1.amazonaws.com/Mainpage/ChrisUyenie_RVP11.jpg',
+            thumbnail: 'https://chrisanduyenie.s3.us-west-1.amazonaws.com/Mainpage/ChrisUyenie_RVP11.jpg',
+            },
+            {
+            original: 'https://chrisanduyenie.s3.us-west-1.amazonaws.com/Mainpage/ChrisUyenie_RVP164.jpg',
+            thumbnail: 'https://chrisanduyenie.s3.us-west-1.amazonaws.com/Mainpage/ChrisUyenie_RVP164.jpg',
+            },
+            {
+            original: 'https://chrisanduyenie.s3.us-west-1.amazonaws.com/Mainpage/ChrisUyenie_RVP168.jpg',
+            thumbnail: 'https://chrisanduyenie.s3.us-west-1.amazonaws.com/Mainpage/ChrisUyenie_RVP168.jpg',
+            },
+            {
+            original: 'https://chrisanduyenie.s3.us-west-1.amazonaws.com/Mainpage/ChrisUyenie_RVP169.jpg',
+            thumbnail: 'https://chrisanduyenie.s3.us-west-1.amazonaws.com/Mainpage/ChrisUyenie_RVP169.jpg',
+            },
+            {
+            original: 'https://chrisanduyenie.s3.us-west-1.amazonaws.com/Mainpage/ChrisUyenie_RVP21.jpg',
+            thumbnail: 'https://chrisanduyenie.s3.us-west-1.amazonaws.com/Mainpage/ChrisUyenie_RVP21.jpg',
+            },
+            {
+            original: 'https://chrisanduyenie.s3.us-west-1.amazonaws.com/Mainpage/ChrisUyenie_RVP26.jpg',
+            thumbnail: 'https://chrisanduyenie.s3.us-west-1.amazonaws.com/Mainpage/ChrisUyenie_RVP26.jpg',
+            },
+            {
+            original: 'https://chrisanduyenie.s3.us-west-1.amazonaws.com/Mainpage/ChrisUyenie_RVP34.jpg',
+            thumbnail: 'https://chrisanduyenie.s3.us-west-1.amazonaws.com/Mainpage/ChrisUyenie_RVP34.jpg',
+            },
+            {
+            original: 'https://chrisanduyenie.s3.us-west-1.amazonaws.com/Mainpage/ChrisUyenie_RVP39.jpg',
+            thumbnail: 'https://chrisanduyenie.s3.us-west-1.amazonaws.com/Mainpage/ChrisUyenie_RVP39.jpg',
+            },
+            {
+            original: 'https://chrisanduyenie.s3.us-west-1.amazonaws.com/Mainpage/ChrisUyenie_RVP48.jpg',
+            thumbnail: 'https://chrisanduyenie.s3.us-west-1.amazonaws.com/Mainpage/ChrisUyenie_RVP48.jpg',
+            },
+            {
+            original: 'https://chrisanduyenie.s3.us-west-1.amazonaws.com/Mainpage/ChrisUyenie_RVP50.jpg',
+            thumbnail: 'https://chrisanduyenie.s3.us-west-1.amazonaws.com/Mainpage/ChrisUyenie_RVP50.jpg',
+            },
+    ];
 
     
+    return (
+        <div>
+            <ImageGallery items={images}
+                startIndex={4}
+                showPlayButton={false}
+                showFullscreenButton={true}
+                lazyLoad={true}
+                thumbnailPosition='right'
+                useBrowserFullscreen={true}
+            />;
+        </div>
 
+    )
 };
 
 
 
-// let random = Math.floor((Math.random() * 50) + 1); 
-
-    // const images = [
-    //     {
-    //         original: 'https://chrisanduyenie.s3.us-west-1.amazonaws.com/Mainpage/ALV00612.jpg',
-    //         thumbnail: 'https://chrisanduyenie.s3.us-west-1.amazonaws.com/Mainpage/ALV00612.jpg',
-    //         },
-    //         {
-    //         original: 'https://chrisanduyenie.s3.us-west-1.amazonaws.com/Mainpage/ALV00643.jpg',
-    //         thumbnail: 'https://chrisanduyenie.s3.us-west-1.amazonaws.com/Mainpage/ALV00643.jpg',
-    //         },
-    //         {
-    //         original: 'https://chrisanduyenie.s3.us-west-1.amazonaws.com/Mainpage/ALV00686.jpg',
-    //         thumbnail: 'https://chrisanduyenie.s3.us-west-1.amazonaws.com/Mainpage/ALV00686.jpg',
-    //         },
-    //         {
-    //         original: 'https://chrisanduyenie.s3.us-west-1.amazonaws.com/Mainpage/ALV00704.jpg',
-    //         thumbnail: 'https://chrisanduyenie.s3.us-west-1.amazonaws.com/Mainpage/ALV00704.jpg',
-    //         },
-    //         {
-    //         original: 'https://chrisanduyenie.s3.us-west-1.amazonaws.com/Mainpage/ALV00739.jpg',
-    //         thumbnail: 'https://chrisanduyenie.s3.us-west-1.amazonaws.com/Mainpage/ALV00739.jpg',
-    //         },
-    //         {
-    //         original: 'https://chrisanduyenie.s3.us-west-1.amazonaws.com/Mainpage/ALV00770.jpg',
-    //         thumbnail: 'https://chrisanduyenie.s3.us-west-1.amazonaws.com/Mainpage/ALV00770.jpg',
-    //         },
-    //         {
-    //         original: 'https://chrisanduyenie.s3.us-west-1.amazonaws.com/Mainpage/ALV00790.jpg',
-    //         thumbnail: 'https://chrisanduyenie.s3.us-west-1.amazonaws.com/Mainpage/ALV00790.jpg',
-    //         },
-    //         {
-    //         original: 'https://chrisanduyenie.s3.us-west-1.amazonaws.com/Mainpage/ALV00837.jpg',
-    //         thumbnail: 'https://chrisanduyenie.s3.us-west-1.amazonaws.com/Mainpage/ALV00837.jpg',
-    //         },
-    //         {
-    //         original: 'https://chrisanduyenie.s3.us-west-1.amazonaws.com/Mainpage/ChrisUyenie_RVP11.jpg',
-    //         thumbnail: 'https://chrisanduyenie.s3.us-west-1.amazonaws.com/Mainpage/ChrisUyenie_RVP11.jpg',
-    //         },
-    //         {
-    //         original: 'https://chrisanduyenie.s3.us-west-1.amazonaws.com/Mainpage/ChrisUyenie_RVP164.jpg',
-    //         thumbnail: 'https://chrisanduyenie.s3.us-west-1.amazonaws.com/Mainpage/ChrisUyenie_RVP164.jpg',
-    //         },
-    //         {
-    //         original: 'https://chrisanduyenie.s3.us-west-1.amazonaws.com/Mainpage/ChrisUyenie_RVP168.jpg',
-    //         thumbnail: 'https://chrisanduyenie.s3.us-west-1.amazonaws.com/Mainpage/ChrisUyenie_RVP168.jpg',
-    //         },
-    //         {
-    //         original: 'https://chrisanduyenie.s3.us-west-1.amazonaws.com/Mainpage/ChrisUyenie_RVP169.jpg',
-    //         thumbnail: 'https://chrisanduyenie.s3.us-west-1.amazonaws.com/Mainpage/ChrisUyenie_RVP169.jpg',
-    //         },
-    //         {
-    //         original: 'https://chrisanduyenie.s3.us-west-1.amazonaws.com/Mainpage/ChrisUyenie_RVP21.jpg',
-    //         thumbnail: 'https://chrisanduyenie.s3.us-west-1.amazonaws.com/Mainpage/ChrisUyenie_RVP21.jpg',
-    //         },
-    //         {
-    //         original: 'https://chrisanduyenie.s3.us-west-1.amazonaws.com/Mainpage/ChrisUyenie_RVP26.jpg',
-    //         thumbnail: 'https://chrisanduyenie.s3.us-west-1.amazonaws.com/Mainpage/ChrisUyenie_RVP26.jpg',
-    //         },
-    //         {
-    //         original: 'https://chrisanduyenie.s3.us-west-1.amazonaws.com/Mainpage/ChrisUyenie_RVP34.jpg',
-    //         thumbnail: 'https://chrisanduyenie.s3.us-west-1.amazonaws.com/Mainpage/ChrisUyenie_RVP34.jpg',
-    //         },
-    //         {
-    //         original: 'https://chrisanduyenie.s3.us-west-1.amazonaws.com/Mainpage/ChrisUyenie_RVP39.jpg',
-    //         thumbnail: 'https://chrisanduyenie.s3.us-west-1.amazonaws.com/Mainpage/ChrisUyenie_RVP39.jpg',
-    //         },
-    //         {
-    //         original: 'https://chrisanduyenie.s3.us-west-1.amazonaws.com/Mainpage/ChrisUyenie_RVP48.jpg',
-    //         thumbnail: 'https://chrisanduyenie.s3.us-west-1.amazonaws.com/Mainpage/ChrisUyenie_RVP48.jpg',
-    //         },
-    //         {
-    //         original: 'https://chrisanduyenie.s3.us-west-1.amazonaws.com/Mainpage/ChrisUyenie_RVP50.jpg',
-    //         thumbnail: 'https://chrisanduyenie.s3.us-west-1.amazonaws.com/Mainpage/ChrisUyenie_RVP50.jpg',
-    //         },
-    // ];
-
-    
-    // return (
-    //     <div>
-    //         <ImageGallery items={images}
-    //             startIndex={4}
-    //             showPlayButton={false}
-    //             showFullscreenButton={true}
-    //             lazyLoad={true}
-    //             thumbnailPosition='right'
-    //             useBrowserFullscreen={true}
-    //         />;
-    //     </div>
-
-    // )
