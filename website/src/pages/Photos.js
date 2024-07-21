4e5a519672b0d6ec1f1bfec38aec9da0f602751f
Swiggityyy <chrisandcommunity@gmail.com>
2024-07-21 13:18:32 -0700

    asdf

--- website/src/pages/Photos.js
+++ website/src/pages/Photos.js
@@ -4,6 +4,7 @@ export default function IndividualIntervalsExample() {
     return (
         <>
             <Headerphoto />
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/S04-Ys_H7AY?si=a2vJimMwVB8gWY_W" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
         </>
     )
 }
