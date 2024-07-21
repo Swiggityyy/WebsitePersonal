e5726514ff78ac61acbdaa2c245a405e33e62e9e
Swiggityyy <chrisandcommunity@gmail.com>
2024-07-21 13:38:42 -0700

    asdf

--- website/src/components/Headerphoto.js
+++ website/src/components/Headerphoto.js
@@ -4,7 +4,7 @@ import "./react-image-gallery/styles/css/image-gallery.css";
 
 export default function Headerphoto() {
 
-
+<iframe width="560" height="315" src="https://www.youtube.com/embed/S04-Ys_H7AY?si=a2vJimMwVB8gWY_W" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
 
 
 // let random = Math.floor((Math.random() * 50) + 1); 
